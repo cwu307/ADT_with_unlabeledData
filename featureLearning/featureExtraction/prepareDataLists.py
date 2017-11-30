b118df049d4da46c70a85e964bc8be60844d8084
cwu307 <cwu307@gatech.edu>
2017-11-29 19:28:30 -0800

    add one missing file

---
+++ featureLearning/featureExtraction/prepareDataLists.py
@@ -0,0 +1,24 @@
+'''
+this script is used to generate file_lists for multiple drum datasets
+'''
+
+
+
+def getEnstDataList(dataDir):
+
+    return dataEnstList
+
+
+
+
+
+def main():
+    return()
+
+if __name__ == "__main__":
+    print('running main() directly')
+    main()
+
+
+
+

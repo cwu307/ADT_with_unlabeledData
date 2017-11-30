344193292290ba996e7c95ef06e9064d433a93a4
cwu307 <cwu307@gatech.edu>
2017-11-29 21:12:40 -0800

    get all the data lists

--- featureLearning/featureExtraction/prepareDataLists.py
+++ featureLearning/featureExtraction/prepareDataLists.py
@@ -2,16 +2,18 @@
 this script is used to generate file_lists for multiple drum datasets
 CW @ GTCMT 2017
 '''
+import numpy as np
 from glob import glob
+from os import listdir
 
 def getEnstDataList(dataDir):
     enstList = []
     folders = ['drummer1', 'drummer2', 'drummer3']
     for folder in folders:
-        audioPath = dataDir + folder + 'audio/'
-        annPath = dataDir + folder + 'annotation/'
-        audioList = glob(audioPath + '*.wav')
-        annList = glob(annPath + '*.txt')
+        audioPath = dataDir + folder + '/audio/'
+        annPath = dataDir + folder + '/annotation/'
+        audioList = sorted(glob((audioPath +  '*.wav')))
+        annList = sorted(glob(annPath + '*.txt'))
         for i in range(0, len(audioList)):
             audioFilename = audioList[i].split('/')[-1].split('_')[0]
             annFilename = annList[i].split('/')[-1].split('_')[0]
@@ -23,8 +25,8 @@ def getRbmaDataList(dataDir):
     rbmaList = []
     audioPath = dataDir + 'audio/'
     annPath = dataDir + 'annotations/'
-    audioList = glob(audioPath + '*.wav')
-    annList = glob(annPath + '*.txt')
+    audioList = sorted(glob(audioPath + '*.wav'))
+    annList = sorted(glob(annPath + '*.txt'))
     for i in range(0, len(audioList)):
         audioFilename = audioList[i].split('/')[-1].split('.')[0]
         annFilename = annList[i].split('/')[-1].split('.')[0]
@@ -36,8 +38,8 @@ def getMdbDataList(dataDir):
     mdbList = []
     audioPath = dataDir + 'audio/full_mix/'
     annPath = dataDir + 'annotations/class/'
-    audioList = glob(audioPath + '*.wav')
-    annList = glob(annPath + '*.txt')
+    audioList = sorted(glob(audioPath + '*.wav'))
+    annList = sorted(glob(annPath + '*.txt'))
     for i in range(0, len(audioList)):
         audioFilename = audioList[i].split('/')[-1].split('_MIX')[0]
         annFilename = annList[i].split('/')[-1].split('_class')[0]
@@ -45,12 +47,12 @@ def getMdbDataList(dataDir):
         mdbList.append((audioList[i], annList[i]))
     return mdbList
 
-def get2005Dir(dataDir):
+def get2005List(dataDir):
     m2005List = []
     audioPath = dataDir + 'audio/'
     annPath = dataDir + 'annotations/'
-    audioList = glob(audioPath + '*.wav')
-    annList = glob(annPath + '*.txt')
+    audioList = sorted(glob(audioPath + '*.wav'))
+    annList = sorted(glob(annPath + '*.txt'))
     for i in range(0, len(audioList)):
         audioFilename = audioList[i].split('/')[-1].split('.')[0]
         annFilename = annList[i].split('/')[-1].split('.')[0]
@@ -58,15 +60,32 @@ def get2005Dir(dataDir):
         m2005List.append((audioList[i], annList[i]))
     return m2005List
 
+def displayList(dataList):
+    for item in dataList:
+        print(item)
+    return()
+
 
 def main():
-    enstDir = '~/../../data/labeled_drum_datasets/CW_ENST_minus_one_wet_new_ratio/'
-    getEnstDataList(enstDir)
-    rbmaDir = '~/../../data/labeled_drum_datasets/2017_mirex/rbma/'
-    m2005Dir = '~/../../data/labeled_drum_datasets/2017_mirex/2005/'
-    mdb = '~/../../data/labeled_drum_datasets/MDBDrums/'
+    enstDir = '/data/labeled_drum_datasets/CW_ENST_minus_one_wet_new_ratio/'
+    enstList = getEnstDataList(enstDir)
+    displayList(enstList)
+    np.save('enstList.npy', enstList)
 
+    rbmaDir = '/data/labeled_drum_datasets/2017_mirex/RBMA/'
+    rbmaList = getRbmaDataList(rbmaDir)
+    displayList(rbmaList)
+    np.save('rbmaList.npy', rbmaList)
 
+    m2005Dir = '/data/labeled_drum_datasets/2017_mirex/2005/'
+    m2005List = get2005List(m2005Dir)
+    displayList(m2005List)
+    np.save('m2005List.npy', m2005List)
+
+    mdbDir = '/data/labeled_drum_datasets/MDB Drums/'
+    mdblist = getMdbDataList(mdbDir)
+    displayList(mdblist)
+    np.save('mdbList.npy', mdblist)
     return()
 
 if __name__ == "__main__":
